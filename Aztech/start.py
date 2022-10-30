a6d04c9b0ab0d20e899f53ae9049c70de1b040b4
KIRITO <115206659+KIRITO-40@users.noreply.github.com>
2022-10-30 15:14:02 +0530

    Delete start.py

--- Aztech/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram.types import Message, filters
-from Aztech import Verify as pbot
-
-PIC = "https://telegra.ph//file/ba29f6d17057c64640c3c.jpg"
-
-@pbot.on_message(filters.command("start"))
-async def start(_, msg:Message):
-    await msg.reply_photo(PIC, caption="Hello")
-     
